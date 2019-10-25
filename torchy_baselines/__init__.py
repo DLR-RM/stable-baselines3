0ad743c85d3c2dbdfab8f18b6c519a45751b4a08
Antonin Raffin <antonin.raffin@dlr.de>
2019-10-25 10:59:15 +0200

    Add A2C

--- torchy_baselines/__init__.py
+++ torchy_baselines/__init__.py
@@ -1,6 +1,7 @@
+from torchy_baselines.a2c import A2C
 from torchy_baselines.cem_rl import CEMRL
 from torchy_baselines.ppo import PPO
 from torchy_baselines.sac import SAC
 from torchy_baselines.td3 import TD3
 
-__version__ = "0.0.4"
+__version__ = "0.0.5a"
---
+++ torchy_baselines/a2c/__init__.py
@@ -0,0 +1,2 @@
+from torchy_baselines.a2c.a2c import A2C
+from torchy_baselines.ppo.policies import MlpPolicy

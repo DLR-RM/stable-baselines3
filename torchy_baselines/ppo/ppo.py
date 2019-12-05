03bf513e5e3be8ae40483e82dc8e92ee1dde601d
Dormann, Noah <Noah.Dormann@dlr.de>
2019-12-05 08:08:20 +0100

    comment refactoring
    Co-Authored-By: Raffin, Antonin <Antonin.Raffin@dlr.de>

--- torchy_baselines/ppo/ppo.py
+++ torchy_baselines/ppo/ppo.py
@@ -317,7 +317,7 @@ class PPO(BaseRLModel):
         """
         Returns a dict of all the optimizers and their parameters
         
-        :return: (Dict) of optimizer names and their state_dict 
+        :return: (dict) of optimizer names and their state_dict 
         """
         return {"opt": self.policy.optimizer.state_dict()}
 

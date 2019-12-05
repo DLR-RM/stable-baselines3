6c423add8d5de35399591067919efa208e907f0c
Antonin Raffin <antonin.raffin@dlr.de>
2019-12-05 16:44:27 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name='torchy_baselines',
       license="MIT",
       long_description="",
       long_description_content_type='text/markdown',
-      version="0.0.6a",
+      version="0.0.7a",
       )
 
 # python setup.py sdist

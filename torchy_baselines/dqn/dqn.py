Finished learn and train function - missing correct loss computation

torchy_baselines/dqn/dqn.py
@@ -84,38 +84,10 @@ class DQN(OffPolicyRLModel):
     def _setup_model(self) -> None:
         super(DQN, self)._setup_model()
 
-    def init_replay_buffer(self,
-                           env: gym.Env,
-                           replay_buffer: ReplayBuffer):
-        """
-        Initially fills the replay buffer with random samples from the environment
-        (only non vector environments supported)
-
-        :param env: (Env) Environments to sample from
-        :param replay_buffer: (ReplayBuffer) ReplayBuffer to fill
-        """
-        replay_buffer.reset()
-        obs = env.reset()
-
-        while not replay_buffer.full:
-
-            action = env.action_space.sample()
-            # Perform action
-            new_obs, reward, done, info = env.step(action)
-
-            if isinstance(self.action_space, gym.spaces.Discrete):
-                # Reshape in case of discrete action
-                action = action.reshape(-1, 1)
-            replay_buffer.add(obs, new_obs, action, reward, done)
-            if done:
-                obs = env.reset()
-            else:
-                obs = new_obs.copy()  # copy the observation here to avoid conflicts
-
     def train(self, gradient_steps: int, batch_size: int = 100, policy_delay: int = 2) -> None:
 
         # Update learning rate according to lr schedule
-        self._update_learning_rate([self.actor.optimizer, self.critic.optimizer])
+        self._update_learning_rate(self.policy.optimizer)
 
         for gradient_step in range(gradient_steps):
 
@@ -123,74 +95,23 @@ class DQN(OffPolicyRLModel):
             replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)
 
             with th.no_grad():
-                # Select action according to policy and add clipped noise
-                noise = replay_data.actions.clone().data.normal_(0, self.target_policy_noise)
-                noise = noise.clamp(-self.target_noise_clip, self.target_noise_clip)
-                next_actions = (self.actor_target(replay_data.next_observations) + noise).clamp(-1, 1)
-
-                # Compute the target Q value
-                target_q1, target_q2 = self.critic_target(replay_data.next_observations, next_actions)
-                target_q = th.min(target_q1, target_q2)
+                # Compute the target Q values
+                target_q = self.policy(replay_data.next_observations)
+                target_q = th.max(target_q, 0)
                 target_q = replay_data.rewards + (1 - replay_data.dones) * self.gamma * target_q
 
             # Get current Q estimates
-            current_q1, current_q2 = self.critic(replay_data.observations, replay_data.actions)
+            current_q = self.critic(replay_data.observations, replay_data.actions)
 
             # Compute critic loss
-            critic_loss = F.mse_loss(current_q1, target_q) + F.mse_loss(current_q2, target_q)
+            loss = F.mse_loss(current_q, target_q)
 
             # Optimize the critic
-            self.critic.optimizer.zero_grad()
-            critic_loss.backward()
-            self.critic.optimizer.step()
-
-            # Delayed policy updates
-            if gradient_step % policy_delay == 0:
-                # Compute actor loss
-                actor_loss = -self.critic.q1_forward(replay_data.observations,
-                                                     self.actor(replay_data.observations)).mean()
-
-                # Optimize the actor
-                self.actor.optimizer.zero_grad()
-                actor_loss.backward()
-                self.actor.optimizer.step()
-
-                # Update the frozen target networks
-                for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
-                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-
-                for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
-                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-
-        self._n_updates += gradient_steps
-        logger.logkv("n_updates", self._n_updates)
-
-    def train(self, n_epochs: int, batch_size: int = 64) -> None:
-        # Update optimizer learning rate
-        self._update_learning_rate(self.policy.optimizer)
-
-        # train for gradient_steps epochs
-        for epoch in range(n_epochs):
-
-            # Sample replay buffer
-            replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)
-
-            # forward observations to get q values
-            with th.no_grad():
-                # Convert to pytorch tensor
-                obs_tensor = th.as_tensor(next_obs).to(self.device)
-                actions = self.policy.forward(obs_tensor)
-            action = action.cpu().numpy()
-            # some how get best q value from that...
-            # compute targets
-            target = rewards + self.gamma * dones * best_q
             self.policy.optimizer.zero_grad()
-            output = self.policy(obs)
-            loss = self.policy.criterion(output, target)
             loss.backward()
             self.policy.optimizer.step()
 
-        self._n_updates += 1
+        self._n_updates += gradient_steps
         logger.logkv("n_updates", self._n_updates)
 
     def learn(self,
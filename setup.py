f171ca6c74c0c55f948302caee317c63a86aa700
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-05-11 17:30:00 +0200

    Use conda env for building the doc

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(name='stable_baselines3',
             # For render
             'opencv-python',
             # For atari games,
-            'atari_py~=0.2.0', 'Pillow'
+            'atari_py~=0.2.0', 'pillow'
         ]
       },
       description='Pytorch version of Stable Baselines, implementations of reinforcement learning algorithms.',

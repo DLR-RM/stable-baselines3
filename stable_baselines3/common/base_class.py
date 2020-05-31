8e80775bfd19b99d8985d4ae6258a0ffba5de255
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-05-31 17:12:11 +0200

    Rename session -> time

--- stable_baselines3/common/base_class.py
+++ stable_baselines3/common/base_class.py
@@ -883,13 +883,13 @@ class OffPolicyRLModel(BaseRLModel):
                 # Log training infos
                 if log_interval is not None and self._episode_num % log_interval == 0:
                     fps = int(self.num_timesteps / (time.time() - self.start_time))
-                    logger.record("rollout/episodes", self._episode_num, exclude="tensorboard")
+                    logger.record("time/episodes", self._episode_num, exclude="tensorboard")
                     if len(self.ep_info_buffer) > 0 and len(self.ep_info_buffer[0]) > 0:
                         logger.record('rollout/ep_rew_mean', self.safe_mean([ep_info['r'] for ep_info in self.ep_info_buffer]))
                         logger.record('rollout/ep_len_mean', self.safe_mean([ep_info['l'] for ep_info in self.ep_info_buffer]))
-                    logger.record("session/fps", fps)
-                    logger.record('session/time_elapsed', int(time.time() - self.start_time), exclude="tensorboard")
-                    logger.record("session/total timesteps", self.num_timesteps, exclude="tensorboard")
+                    logger.record("time/fps", fps)
+                    logger.record('time/time_elapsed', int(time.time() - self.start_time), exclude="tensorboard")
+                    logger.record("time/total timesteps", self.num_timesteps, exclude="tensorboard")
                     if self.use_sde:
                         logger.record("train/std", (self.actor.get_std()).mean().item())
 
--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -328,15 +328,15 @@ class PPO(BaseRLModel):
             # Log training infos
             if log_interval is not None and iteration % log_interval == 0:
                 fps = int(self.num_timesteps / (time.time() - self.start_time))
-                logger.record("rollout/iterations", iteration, exclude="tensorboard")
+                logger.record("time/iterations", iteration, exclude="tensorboard")
                 if len(self.ep_info_buffer) > 0 and len(self.ep_info_buffer[0]) > 0:
                     logger.record("rollout/ep_rew_mean",
                                   self.safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer]))
                     logger.record("rollout/ep_len_mean",
                                   self.safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer]))
-                logger.record("session/fps", fps)
-                logger.record("session/time_elapsed", int(time.time() - self.start_time), exclude="tensorboard")
-                logger.record("session/total timesteps", self.num_timesteps, exclude="tensorboard")
+                logger.record("time/fps", fps)
+                logger.record("time/time_elapsed", int(time.time() - self.start_time), exclude="tensorboard")
+                logger.record("time/total timesteps", self.num_timesteps, exclude="tensorboard")
                 logger.dump(step=self.num_timesteps)
 
             self.train(self.n_epochs, batch_size=self.batch_size)

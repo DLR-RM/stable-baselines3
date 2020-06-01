38bda1fa8c1a2da4f4f0769354bd4413c303ae39
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-06-01 13:49:01 +0200

    Fix for tensorboard name

--- stable_baselines3/dqn/dqn.py
+++ stable_baselines3/dqn/dqn.py
@@ -175,7 +175,8 @@ class DQN(OffPolicyRLModel):
               reset_num_timesteps: bool = True) -> OffPolicyRLModel:
 
         total_timesteps, callback = self._setup_learn(total_timesteps, eval_env, callback, eval_freq,
-                                                      n_eval_episodes, eval_log_path, reset_num_timesteps)
+                                                      n_eval_episodes, eval_log_path, reset_num_timesteps,
+                                                      tb_log_name)
 
         callback.on_training_start(locals(), globals())
 

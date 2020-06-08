11d33eb4aee152fcb99dfec3c9086c66244868f5
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-06-08 11:15:10 +0200

    Fix gSDE loading issue in test mode (#45)
    * Fix gSDE loading issue in test mode
    
    * Forward `reset_noise` method
    
    * Re-add `make_actor`
    
    * Reformat

--- stable_baselines3/common/base_class.py
+++ stable_baselines3/common/base_class.py
@@ -375,6 +375,10 @@ class BaseRLModel(ABC):
             for name in tensors:
                 recursive_setattr(model, name, tensors[name])
 
+        # Sample gSDE exploration matrix, so it uses the right device
+        # see issue #44
+        if model.use_sde:
+            model.policy.reset_noise()
         return model
 
     @staticmethod

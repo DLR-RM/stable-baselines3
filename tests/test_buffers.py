3907ef6d9d597af12e704a036b434210b7ae1173
Stelios Tymvios <stelios.tymvios@icloud.com>
2020-07-04 15:32:23 +0300

    more tests, corrected a bug incrementing was wrong when done=1

--- tests/test_buffers.py
+++ tests/test_buffers.py
@@ -65,3 +65,20 @@ def test_nsteps():
     # shouldn't be able to get batch with indice 5 because the pointer is at 5
     with pytest.raises(AssertionError):
         buffer._get_samples(np.array([5]))
+    
+    buffer = NstepReplayBuffer(10, spaces.Discrete(5), spaces.Discrete(5), n_step=5, gamma=0.9)
+    buffer.add(0, 1, 10, 1, 1)
+    buffer.add(1, 2, 11, 1, 1)
+    buffer.add(2, 3, 12, 1, 1)
+    buffer.add(3, 4, 13, 1, 1)
+    buffer.add(4, 5, 14, 1, 1)
+    obs, act, next_obs, dones, rewards = buffer._get_samples(np.array([1, 2, 3, 4]))
+    assert obs.shape == (4, 1)
+    assert act.shape == (4, 1)
+    assert next_obs.shape == (4, 1)
+    assert dones.shape == (4, 1)
+    assert rewards.shape == (4, 1)
+    assert np.allclose(dones, np.ones_like(dones))
+    assert np.allclose(rewards, np.array([1, 1, 1, 1]).reshape(4, 1))
+    assert np.allclose(act, np.array([11, 12, 13, 14]).reshape(4, 1))
+    assert np.allclose(next_obs, np.array([[2], [3], [4], [5]]))

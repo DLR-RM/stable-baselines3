a58fb991e4c34b18ccbef42d11c909e42a966662
Anssi 'Miffyli' Kanervisto <kaneran21@hotmail.com>
2020-07-17 01:38:43 +0300

    Fix number of filters in NatureCNN network

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -74,7 +74,7 @@ class NatureCNN(BaseFeaturesExtractor):
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
             nn.ReLU(),
-            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.Flatten(),
         )

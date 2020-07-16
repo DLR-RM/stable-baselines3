e09936442af6f600897a0ddd0e2fe8ae52b55bba
Stelios Tymvios <stelios.tymvios@icloud.com>
2020-07-16 10:47:33 +0300

    rerverted autofmt

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,16 +12,18 @@ from stable_baselines3.common.atari_wrappers import ClipRewardEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.cmd_util import make_vec_env, make_atari_env
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.noise import VectorizedActionNoise, OrnsteinUhlenbeckActionNoise, ActionNoise
-from stable_baselines3.common.utils import polyak_update
+from stable_baselines3.common.noise import (VectorizedActionNoise,
+                                            OrnsteinUhlenbeckActionNoise, ActionNoise)
+from stable_baselines3.common.utils  import polyak_update
 
 
-@pytest.mark.parametrize("env_id", ["CartPole-v1", lambda: gym.make("CartPole-v1")])
+@pytest.mark.parametrize("env_id", ['CartPole-v1', lambda: gym.make('CartPole-v1')])
 @pytest.mark.parametrize("n_envs", [1, 2])
 @pytest.mark.parametrize("vec_env_cls", [None, SubprocVecEnv])
 @pytest.mark.parametrize("wrapper_class", [None, gym.wrappers.TimeLimit])
 def test_make_vec_env(env_id, n_envs, vec_env_cls, wrapper_class):
-    env = make_vec_env(env_id, n_envs, vec_env_cls=vec_env_cls, wrapper_class=wrapper_class, monitor_dir=None, seed=0)
+    env = make_vec_env(env_id, n_envs, vec_env_cls=vec_env_cls,
+                       wrapper_class=wrapper_class, monitor_dir=None, seed=0)
 
     assert env.num_envs == n_envs
 
@@ -37,12 +39,13 @@ def test_make_vec_env(env_id, n_envs, vec_env_cls, wrapper_class):
     env.close()
 
 
-@pytest.mark.parametrize("env_id", ["BreakoutNoFrameskip-v4"])
+@pytest.mark.parametrize("env_id", ['BreakoutNoFrameskip-v4'])
 @pytest.mark.parametrize("n_envs", [1, 2])
 @pytest.mark.parametrize("wrapper_kwargs", [None, dict(clip_reward=False, screen_size=60)])
 def test_make_atari_env(env_id, n_envs, wrapper_kwargs):
-    env_id = "BreakoutNoFrameskip-v4"
-    env = make_atari_env(env_id, n_envs, wrapper_kwargs=wrapper_kwargs, monitor_dir=None, seed=0)
+    env_id = 'BreakoutNoFrameskip-v4'
+    env = make_atari_env(env_id, n_envs,
+                         wrapper_kwargs=wrapper_kwargs, monitor_dir=None, seed=0)
 
     assert env.num_envs == n_envs
 
@@ -69,15 +72,10 @@ def test_custom_vec_env(tmp_path):
     """
     Stand alone test for a special case (passing a custom VecEnv class) to avoid doubling the number of tests.
     """
-    monitor_dir = tmp_path / "test_make_vec_env/"
-    env = make_vec_env(
-        "CartPole-v1",
-        n_envs=1,
-        monitor_dir=monitor_dir,
-        seed=0,
-        vec_env_cls=SubprocVecEnv,
-        vec_env_kwargs={"start_method": None},
-    )
+    monitor_dir = tmp_path / 'test_make_vec_env/'
+    env = make_vec_env('CartPole-v1', n_envs=1,
+                       monitor_dir=monitor_dir, seed=0,
+                       vec_env_cls=SubprocVecEnv, vec_env_kwargs={'start_method': None})
 
     assert env.num_envs == 1
     assert isinstance(env, SubprocVecEnv)
@@ -89,27 +87,20 @@ def test_custom_vec_env(tmp_path):
 
     # This should fail because DummyVecEnv does not have any keyword argument
     with pytest.raises(TypeError):
-        make_vec_env("CartPole-v1", n_envs=1, vec_env_kwargs={"dummy": False})
+        make_vec_env('CartPole-v1', n_envs=1, vec_env_kwargs={'dummy': False})
 
 
 def test_evaluate_policy():
-    model = A2C("MlpPolicy", "Pendulum-v0", seed=0)
+    model = A2C('MlpPolicy', 'Pendulum-v0', seed=0)
     n_steps_per_episode, n_eval_episodes = 200, 2
     model.n_callback_calls = 0
 
     def dummy_callback(locals_, _globals):
-        locals_["model"].n_callback_calls += 1
+        locals_['model'].n_callback_calls += 1
 
-    _, episode_lengths = evaluate_policy(
-        model,
-        model.get_env(),
-        n_eval_episodes,
-        deterministic=True,
-        render=False,
-        callback=dummy_callback,
-        reward_threshold=None,
-        return_episode_rewards=True,
-    )
+    _, episode_lengths = evaluate_policy(model, model.get_env(), n_eval_episodes, deterministic=True,
+                                         render=False, callback=dummy_callback, reward_threshold=None,
+                                         return_episode_rewards=True)
 
     n_steps = sum(episode_lengths)
     assert n_steps == n_steps_per_episode * n_eval_episodes

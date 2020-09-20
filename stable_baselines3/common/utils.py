004307a22c0d0dc04112730602fdd4bf90100509
Lior Cohen <liorcohen5@gmail.com>
2020-09-20 13:47:16 +0300

    PR fixes: bug fix - a non-related test failed when running on GPU. updated the assertion to consider only types of devices. Also corrected a related bug in 'get_device()' method.

--- stable_baselines3/common/utils.py
+++ stable_baselines3/common/utils.py
@@ -145,7 +145,7 @@ def get_device(device: Union[th.device, str] = "auto") -> th.device:
     device = th.device(device)
 
     # Cuda not available
-    if device == th.device("cuda") and not th.cuda.is_available():
+    if device.type == th.device("cuda").type and not th.cuda.is_available():
         return th.device("cpu")
 
     return device

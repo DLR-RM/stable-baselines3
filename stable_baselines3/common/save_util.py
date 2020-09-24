44a723eecb443271d05c9bed7844c0401fa6a93a
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-09-24 16:05:36 +0200

    Fix loading of old versions and update changelog (#165)

--- stable_baselines3/common/save_util.py
+++ stable_baselines3/common/save_util.py
@@ -404,7 +404,8 @@ def load_from_zip_file(
                     # Load the parameters with the right ``map_location``.
                     # Remove ".pth" ending with splitext
                     th_object = th.load(file_content, map_location=device)
-                    if file_path == "pytorch_variables.pth":
+                    # "tensors.pth" was renamed "pytorch_variables.pth" in v0.9.0, see PR #138
+                    if file_path == "pytorch_variables.pth" or file_path == "tensors.pth":
                         # PyTorch variables (not state_dicts)
                         pytorch_variables = th_object
                     else:

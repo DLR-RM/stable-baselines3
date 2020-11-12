c2c9770074a370f193b8930df60536332be945c8
Anssi "Miffyli" Kanervisto <kaneran21@hotmail.com>
2020-11-13 00:50:49 +0200

    Remove unnecessary typing trickery

--- stable_baselines3/common/monitor.py
+++ stable_baselines3/common/monitor.py
@@ -4,7 +4,6 @@ import csv
 import json
 import os
 import time
-import typing
 from glob import glob
 from typing import List, Optional, Tuple, Union
 
@@ -12,8 +11,7 @@ import gym
 import numpy as np
 import pandas
 
-if typing.TYPE_CHECKING:
-    from stable_baselines3.common.type_aliases import GymObs, GymStepReturn
+from stable_baselines3.common.type_aliases import GymObs, GymStepReturn
 
 
 class Monitor(gym.Wrapper):
@@ -66,7 +64,7 @@ class Monitor(gym.Wrapper):
         self.total_steps = 0
         self.current_reset_info = {}  # extra info about the current episode, that was passed in during reset()
 
-    def reset(self, **kwargs) -> "GymObs":
+    def reset(self, **kwargs) -> GymObs:
         """
         Calls the Gym environment reset. Can only be called if the environment is over, or if allow_early_resets is True
 
@@ -87,7 +85,7 @@ class Monitor(gym.Wrapper):
             self.current_reset_info[key] = value
         return self.env.reset(**kwargs)
 
-    def step(self, action: Union[np.ndarray, int]) -> "GymStepReturn":
+    def step(self, action: Union[np.ndarray, int]) -> GymStepReturn:
         """
         Step the environment with the given action
 

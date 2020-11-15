a7b1dde75f34386df3ebec1d3646bacfb868787f
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-11-15 20:09:19 +0100

    Remove unused imports

--- stable_baselines3/common/utils.py
+++ stable_baselines3/common/utils.py
@@ -3,7 +3,7 @@ import os
 import random
 from collections import deque
 from itertools import zip_longest
-from typing import Callable, Iterable, Optional, Union
+from typing import Iterable, Optional, Union
 
 import gym
 import numpy as np

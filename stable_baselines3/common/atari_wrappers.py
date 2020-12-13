14ea6a687549ecf5a77395edb886d9ca7c8ee081
Lucas Alegre <lucasnale@gmail.com>
2020-12-13 12:10:54 -0300

    Fixed docstring of AtariWrapper which was not inside of __init__

--- stable_baselines3/common/atari_wrappers.py
+++ stable_baselines3/common/atari_wrappers.py
@@ -199,28 +199,6 @@ class WarpFrame(gym.ObservationWrapper):
 
 
 class AtariWrapper(gym.Wrapper):
-    """
-    Atari 2600 preprocessings
-
-    Specifically:
-
-    * NoopReset: obtain initial state by taking random number of no-ops on reset.
-    * Frame skipping: 4 by default
-    * Max-pooling: most recent two observations
-    * Termination signal when a life is lost.
-    * Resize to a square image: 84x84 by default
-    * Grayscale observation
-    * Clip reward to {-1, 0, 1}
-
-    :param env: gym environment
-    :param noop_max:: max number of no-ops
-    :param frame_skip:: the frequency at which the agent experiences the game.
-    :param screen_size:: resize Atari frame
-    :param terminal_on_life_loss:: if True, then step() returns done=True whenever a
-            life is lost.
-    :param clip_reward: If True (default), the reward is clip to {-1, 0, 1} depending on its sign.
-    """
-
     def __init__(
         self,
         env: gym.Env,
@@ -230,6 +208,27 @@ class AtariWrapper(gym.Wrapper):
         terminal_on_life_loss: bool = True,
         clip_reward: bool = True,
     ):
+        """
+        Atari 2600 preprocessings
+
+        Specifically:
+
+        * NoopReset: obtain initial state by taking random number of no-ops on reset.
+        * Frame skipping: 4 by default
+        * Max-pooling: most recent two observations
+        * Termination signal when a life is lost.
+        * Resize to a square image: 84x84 by default
+        * Grayscale observation
+        * Clip reward to {-1, 0, 1}
+
+        :param env: gym environment
+        :param noop_max:: max number of no-ops
+        :param frame_skip:: the frequency at which the agent experiences the game.
+        :param screen_size:: resize Atari frame
+        :param terminal_on_life_loss:: if True, then step() returns done=True whenever a
+                life is lost.
+        :param clip_reward: If True (default), the reward is clip to {-1, 0, 1} depending on its sign.
+        """
         env = NoopResetEnv(env, noop_max=noop_max)
         env = MaxAndSkipEnv(env, skip=frame_skip)
         if terminal_on_life_loss:

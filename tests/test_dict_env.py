4f787fa54cb5e7c9ca0137f695db7b5bade1262b
Antonin Raffin <antonin.raffin@ensta.org>
2021-03-09 11:32:53 +0100

    Add consistency test

--- tests/test_dict_env.py
+++ tests/test_dict_env.py
@@ -15,7 +15,13 @@ class DummyDictEnv(gym.Env):
 
     metadata = {"render.modes": ["human"]}
 
-    def __init__(self, use_discrete_actions=False, channel_last=False, nested_dict_obs=False):
+    def __init__(
+        self,
+        use_discrete_actions=False,
+        channel_last=False,
+        nested_dict_obs=False,
+        vec_only=False,
+    ):
         super().__init__()
         if use_discrete_actions:
             self.action_space = spaces.Discrete(3)
@@ -41,9 +47,22 @@ class DummyDictEnv(gym.Env):
             }
         )
 
+        # For checking consistency with normal MlpPolicy
+        if vec_only:
+            self.observation_space = spaces.Dict(
+                {
+                    # Vector obs
+                    "vec": spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32),
+                }
+            )
+
         if nested_dict_obs:
             # Add dictionary observation inside observation space
             self.observation_space.spaces["nested-dict"] = spaces.Dict({"nested-dict-discrete": spaces.Discrete(4)})
+
+    def seed(self, seed=None):
+        if seed is not None:
+            self.observation_space.seed(seed)
 
     def step(self, action):
         reward = 0.0
@@ -58,6 +77,52 @@ class DummyDictEnv(gym.Env):
 
     def render(self, mode="human"):
         pass
+
+
+@pytest.mark.parametrize("model_class", [PPO, A2C, DQN, DDPG, SAC, TD3])
+def test_consistency(model_class):
+    """
+    Make sure that dict obs with vector only vs using flatten obs is equivalent.
+    This ensures notable that the network architectures are the same.
+    """
+    use_discrete_actions = model_class == DQN
+    dict_env = DummyDictEnv(use_discrete_actions=use_discrete_actions, vec_only=True)
+    dict_env = gym.wrappers.TimeLimit(dict_env, 100)
+    env = gym.wrappers.FlattenObservation(dict_env)
+    dict_env.seed(10)
+    obs = dict_env.reset()
+
+    kwargs = {}
+    n_steps = 256
+
+    if model_class in {A2C, PPO}:
+        kwargs = dict(
+            n_steps=128,
+        )
+    else:
+        # Avoid memory error when using replay buffer
+        # Reduce the size of the features and make learning faster
+        kwargs = dict(
+            buffer_size=250,
+            train_freq=8,
+            gradient_steps=1,
+        )
+        if model_class == DQN:
+            kwargs["learning_starts"] = 0
+
+    dict_model = model_class("MultiInputPolicy", dict_env, gamma=0.5, seed=1, **kwargs)
+    action_before_learning_1, _ = dict_model.predict(obs, deterministic=True)
+    dict_model.learn(total_timesteps=n_steps)
+
+    normal_model = model_class("MlpPolicy", env, gamma=0.5, seed=1, **kwargs)
+    action_before_learning_2, _ = normal_model.predict(obs["vec"], deterministic=True)
+    normal_model.learn(total_timesteps=n_steps)
+
+    action_1, _ = dict_model.predict(obs, deterministic=True)
+    action_2, _ = normal_model.predict(obs["vec"], deterministic=True)
+
+    assert np.allclose(action_before_learning_1, action_before_learning_2)
+    assert np.allclose(action_1, action_2)
 
 
 @pytest.mark.parametrize("model_class", [PPO, A2C, DQN, DDPG, SAC, TD3])

0bcfa11b9bb97f204e40cd23536300d14a26b9ab
Anssi "Miffyli" Kanervisto <kaneran21@hotmail.com>
2021-04-23 02:13:34 +0300

    Update default CNN feature vector size

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -236,10 +236,11 @@ class CombinedExtractor(BaseFeaturesExtractor):
     the output features are concatenated and fed through additional MLP network ("combined").
 
     :param observation_space:
-    :param cnn_output_dim: Number of features to output from each CNN submodule(s)
+    :param cnn_output_dim: Number of features to output from each CNN submodule(s). Defaults to
+        256 to avoid exploding network sizes.
     """
 
-    def __init__(self, observation_space: gym.spaces.Dict, cnn_output_dim: int = 64):
+    def __init__(self, observation_space: gym.spaces.Dict, cnn_output_dim: int = 256):
         # TODO we do not know features-dim here before going over all the items, so put something there. This is dirty!
         super(CombinedExtractor, self).__init__(observation_space, features_dim=1)
 

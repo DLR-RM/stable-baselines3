a052a338431cd38486d813df8cc808d4e89703bc
Antonin Raffin <antonin.raffin@ensta.org>
2021-05-04 09:54:19 +0200

    Fix pytype error

--- stable_baselines3/common/base_class.py
+++ stable_baselines3/common/base_class.py
@@ -376,7 +376,7 @@ class BaseAlgorithm(ABC):
 
         # Avoid resetting the environment when calling ``.learn()`` consecutive times
         if reset_num_timesteps or self._last_obs is None:
-            self._last_obs = self.env.reset()
+            self._last_obs = self.env.reset()  # pytype: disable=annotation-type-mismatch
             self._last_episode_starts = np.ones((self.env.num_envs,), dtype=bool)
             # Retrieve unnormalized observation for saving into the buffer
             if self._vec_normalize_env is not None:

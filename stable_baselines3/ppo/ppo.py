b5549864a2e6237ce7e9104c4af1544e3ba23822
09tangriro <rkt16@ic.ac.uk>
2021-05-07 15:56:16 +0100

    r -> log_ratio

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -237,8 +237,8 @@ class PPO(OnPolicyAlgorithm):
                 # see issue #417: https://github.com/DLR-RM/stable-baselines3/issues/417
                 # and Schulman blog: http://joschu.net/blog/kl-approx.html
                 with th.no_grad():
-                    r = rollout_data.old_log_prob - log_prob
-                    approx_kl_div = th.mean((th.exp(r) - 1) - r).cpu().numpy()
+                    log_ratio = rollout_data.old_log_prob - log_prob
+                    approx_kl_div = th.mean((th.exp(log_ratio) - 1) - log_ratio).cpu().numpy()
                     approx_kl_divs.append(approx_kl_div)
 
                 if self.target_kl is not None and approx_kl_div > 1.5 * self.target_kl:

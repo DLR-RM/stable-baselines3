67a329ac964830753826a62e67b4623a5406a55b
Antonin RAFFIN <antonin.raffin@ensta.org>
2021-05-12 11:21:54 +0200

    Fix ent coef loading for SAC (#429)
    * Fix ent coef loading for SAC
    
    * Better fix and add comment

--- stable_baselines3/common/base_class.py
+++ stable_baselines3/common/base_class.py
@@ -665,6 +665,13 @@ class BaseAlgorithm(ABC):
         # put other pytorch variables back in place
         if pytorch_variables is not None:
             for name in pytorch_variables:
+                # Skip if PyTorch variable was not defined (to ensure backward compatibility).
+                # This happens when using SAC/TQC.
+                # SAC has an entropy coefficient which can be fixed or optimized.
+                # If it is optimized, an additional PyTorch variable `log_ent_coef` is defined,
+                # otherwise it is initialized to `None`.
+                if pytorch_variables[name] is None:
+                    continue
                 # Set the data attribute directly to avoid issue when using optimizers
                 # See https://github.com/DLR-RM/stable-baselines3/issues/391
                 recursive_setattr(model, name + ".data", pytorch_variables[name].data)

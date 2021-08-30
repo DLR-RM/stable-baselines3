7d4c6dd40f2f0eb1caa49a1eb7ad64356419b35c
Antonin Raffin <antonin.raffin@ensta.org>
2021-08-30 16:40:23 +0200

    Refactor tests

--- tests/test_train_eval_mode.py
+++ tests/test_train_eval_mode.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import gym
 import numpy as np
 import pytest
@@ -119,6 +121,19 @@ def clone_sac_batch_norm_stats(
     critic_target_bias, critic_target_running_mean = clone_batch_norm_stats(critic_target_batch_norm)
 
     return (actor_bias, actor_running_mean, critic_bias, critic_running_mean, critic_target_bias, critic_target_running_mean)
+
+
+def clone_on_policy_batch_norm(model: Union[A2C, PPO]) -> (th.Tensor, th.Tensor):
+    return clone_batch_norm_stats(model.policy.features_extractor.batch_norm)
+
+
+CLONE_HELPERS = {
+    A2C: clone_on_policy_batch_norm,
+    DQN: clone_dqn_batch_norm_stats,
+    SAC: clone_sac_batch_norm_stats,
+    TD3: clone_td3_batch_norm_stats,
+    PPO: clone_on_policy_batch_norm,
+}
 
 
 def test_dqn_train_with_batch_norm():
@@ -251,135 +266,45 @@ def test_a2c_ppo_train_with_batch_norm(model_class, env_id):
         seed=1,
     )
 
-    batch_norm = model.policy.features_extractor.batch_norm
-    bias_before, running_mean_before = clone_batch_norm_stats(batch_norm)
+    bias_before, running_mean_before = clone_on_policy_batch_norm(model)
 
     model.learn(total_timesteps=200)
 
-    bias_after, running_mean_after = clone_batch_norm_stats(batch_norm)
+    bias_after, running_mean_after = clone_on_policy_batch_norm(model)
 
     assert ~th.isclose(bias_before, bias_after).all()
     assert ~th.isclose(running_mean_before, running_mean_after).all()
 
 
-def test_dqn_collect_rollouts_with_batch_norm():
-    learning_starts = 200
-    model = DQN(
+@pytest.mark.parametrize("model_class", [DQN, TD3, SAC])
+def test_offpolicy_collect_rollout_batch_norm(model_class):
+    if model_class in [DQN]:
+        env_id = "CartPole-v1"
+    else:
+        env_id = "Pendulum-v0"
+
+    clone_helper = CLONE_HELPERS[model_class]
+
+    learning_starts = 10
+    model = model_class(
         "MlpPolicy",
-        "CartPole-v1",
+        env_id,
         policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
         learning_starts=learning_starts,
         seed=1,
+        gradient_steps=0,
+        train_freq=1,
     )
 
-    (
-        q_net_bias_before,
-        q_net_running_mean_before,
-        q_net_target_bias_before,
-        q_net_target_running_mean_before,
-    ) = clone_dqn_batch_norm_stats(model)
+    batch_norm_stats_before = clone_helper(model)
 
-    model.learn(total_timesteps=learning_starts)
+    model.learn(total_timesteps=100)
 
-    (
-        q_net_bias_after,
-        q_net_running_mean_after,
-        q_net_target_bias_after,
-        q_net_target_running_mean_after,
-    ) = clone_dqn_batch_norm_stats(model)
+    batch_norm_stats_after = clone_helper(model)
 
-    assert th.isclose(q_net_bias_before, q_net_bias_after).all()
-    assert th.isclose(q_net_running_mean_before, q_net_running_mean_after).all()
-
-    assert th.isclose(q_net_target_bias_before, q_net_target_bias_after).all()
-    assert th.isclose(q_net_target_running_mean_before, q_net_target_running_mean_after).all()
-
-
-def test_td3_collect_rollouts_with_batch_norm():
-    learning_starts = 200
-    model = TD3(
-        "MlpPolicy",
-        "Pendulum-v0",
-        policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
-        learning_starts=learning_starts,
-        seed=1,
-    )
-
-    (
-        actor_bias_before,
-        actor_running_mean_before,
-        critic_bias_before,
-        critic_running_mean_before,
-        actor_target_bias_before,
-        actor_target_running_mean_before,
-        critic_target_bias_before,
-        critic_target_running_mean_before,
-    ) = clone_td3_batch_norm_stats(model)
-
-    model.learn(total_timesteps=learning_starts)
-
-    (
-        actor_bias_after,
-        actor_running_mean_after,
-        critic_bias_after,
-        critic_running_mean_after,
-        actor_target_bias_after,
-        actor_target_running_mean_after,
-        critic_target_bias_after,
-        critic_target_running_mean_after,
-    ) = clone_td3_batch_norm_stats(model)
-
-    assert th.isclose(actor_bias_before, actor_bias_after).all()
-    assert th.isclose(actor_running_mean_before, actor_running_mean_after).all()
-
-    assert th.isclose(critic_bias_before, critic_bias_after).all()
-    assert th.isclose(critic_running_mean_before, critic_running_mean_after).all()
-
-    assert th.isclose(actor_target_bias_before, actor_target_bias_after).all()
-    assert th.isclose(actor_target_running_mean_before, actor_target_running_mean_after).all()
-
-    assert th.isclose(critic_target_bias_before, critic_target_bias_after).all()
-    assert th.isclose(critic_target_running_mean_before, critic_target_running_mean_after).all()
-
-
-def test_sac_collect_rollouts_with_batch_norm():
-    learning_starts = 200
-    model = SAC(
-        "MlpPolicy",
-        "Pendulum-v0",
-        policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
-        learning_starts=learning_starts,
-        seed=1,
-    )
-
-    (
-        actor_bias_before,
-        actor_running_mean_before,
-        critic_bias_before,
-        critic_running_mean_before,
-        critic_target_bias_before,
-        critic_target_running_mean_before,
-    ) = clone_sac_batch_norm_stats(model)
-
-    model.learn(total_timesteps=learning_starts)
-
-    (
-        actor_bias_after,
-        actor_running_mean_after,
-        critic_bias_after,
-        critic_running_mean_after,
-        critic_target_bias_after,
-        critic_target_running_mean_after,
-    ) = clone_sac_batch_norm_stats(model)
-
-    assert th.isclose(actor_bias_before, actor_bias_after).all()
-    assert th.isclose(actor_running_mean_before, actor_running_mean_after).all()
-
-    assert th.isclose(critic_bias_before, critic_bias_after).all()
-    assert th.isclose(critic_running_mean_before, critic_running_mean_after).all()
-
-    assert th.isclose(critic_target_bias_before, critic_target_bias_after).all()
-    assert th.isclose(critic_target_running_mean_before, critic_target_running_mean_after).all()
+    # No change in batch norm params
+    for param_before, param_after in zip(batch_norm_stats_before, batch_norm_stats_after):
+        assert th.isclose(param_before, param_after).all()
 
 
 @pytest.mark.parametrize("model_class", [A2C, PPO])
@@ -390,143 +315,20 @@ def test_a2c_ppo_collect_rollouts_with_batch_norm(model_class, env_id):
         env_id,
         policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
         seed=1,
+        n_steps=64,
     )
 
-    batch_norm = model.policy.features_extractor.batch_norm
-    bias_before, running_mean_before = clone_batch_norm_stats(batch_norm)
+    bias_before, running_mean_before = clone_on_policy_batch_norm(model)
 
-    total_timesteps, callback = model._setup_learn(total_timesteps=100, eval_env=model.get_env())
+    total_timesteps, callback = model._setup_learn(total_timesteps=2 * 64, eval_env=model.get_env())
 
-    for _ in range(10):
+    for _ in range(2):
         model.collect_rollouts(model.get_env(), callback, model.rollout_buffer, n_rollout_steps=model.n_steps)
 
-    bias_after, running_mean_after = clone_batch_norm_stats(batch_norm)
+    bias_after, running_mean_after = clone_on_policy_batch_norm(model)
 
     assert th.isclose(bias_before, bias_after).all()
     assert th.isclose(running_mean_before, running_mean_after).all()
-
-
-def test_dqn_predict_with_batch_norm():
-    model = DQN(
-        "MlpPolicy",
-        "CartPole-v1",
-        policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
-        seed=1,
-    )
-
-    (
-        q_net_bias_before,
-        q_net_running_mean_before,
-        q_net_target_bias_before,
-        q_net_target_running_mean_before,
-    ) = clone_dqn_batch_norm_stats(model)
-
-    env = model.get_env()
-    observation = env.reset()
-    for _ in range(10):
-        model.predict(observation, deterministic=True)
-
-    (
-        q_net_bias_after,
-        q_net_running_mean_after,
-        q_net_target_bias_after,
-        q_net_target_running_mean_after,
-    ) = clone_dqn_batch_norm_stats(model)
-
-    assert th.isclose(q_net_bias_before, q_net_bias_after).all()
-    assert th.isclose(q_net_running_mean_before, q_net_running_mean_after).all()
-
-    assert th.isclose(q_net_target_bias_before, q_net_target_bias_after).all()
-    assert th.isclose(q_net_target_running_mean_before, q_net_target_running_mean_after).all()
-
-
-def test_td3_predict_with_batch_norm():
-    model = TD3(
-        "MlpPolicy",
-        "Pendulum-v0",
-        policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
-        seed=1,
-    )
-
-    (
-        actor_bias_before,
-        actor_running_mean_before,
-        critic_bias_before,
-        critic_running_mean_before,
-        actor_target_bias_before,
-        actor_target_running_mean_before,
-        critic_target_bias_before,
-        critic_target_running_mean_before,
-    ) = clone_td3_batch_norm_stats(model)
-
-    env = model.get_env()
-    observation = env.reset()
-    for _ in range(10):
-        model.predict(observation, deterministic=True)
-
-    (
-        actor_bias_after,
-        actor_running_mean_after,
-        critic_bias_after,
-        critic_running_mean_after,
-        actor_target_bias_after,
-        actor_target_running_mean_after,
-        critic_target_bias_after,
-        critic_target_running_mean_after,
-    ) = clone_td3_batch_norm_stats(model)
-
-    assert th.isclose(actor_bias_before, actor_bias_after).all()
-    assert th.isclose(actor_running_mean_before, actor_running_mean_after).all()
-
-    assert th.isclose(critic_bias_before, critic_bias_after).all()
-    assert th.isclose(critic_running_mean_before, critic_running_mean_after).all()
-
-    assert th.isclose(actor_target_bias_before, actor_target_bias_after).all()
-    assert th.isclose(actor_target_running_mean_before, actor_target_running_mean_after).all()
-
-    assert th.isclose(critic_target_bias_before, critic_target_bias_after).all()
-    assert th.isclose(critic_target_running_mean_before, critic_target_running_mean_after).all()
-
-
-def test_sac_predict_with_batch_norm():
-    model = SAC(
-        "MlpPolicy",
-        "Pendulum-v0",
-        policy_kwargs=dict(net_arch=[16, 16], features_extractor_class=FlattenBatchNormDropoutExtractor),
-        seed=1,
-    )
-
-    (
-        actor_bias_before,
-        actor_running_mean_before,
-        critic_bias_before,
-        critic_running_mean_before,
-        critic_target_bias_before,
-        critic_target_running_mean_before,
-    ) = clone_sac_batch_norm_stats(model)
-
-    env = model.get_env()
-    observation = env.reset()
-    for _ in range(10):
-        model.predict(observation, deterministic=True)
-
-    (
-        actor_bias_after,
-        actor_running_mean_after,
-        critic_bias_after,
-        critic_running_mean_after,
-        critic_target_bias_after,
-        critic_target_running_mean_after,
-    ) = clone_sac_batch_norm_stats(model)
-
-    assert th.isclose(actor_bias_before, actor_bias_after).all()
-    assert th.isclose(actor_running_mean_before, actor_running_mean_after).all()
-
-    assert th.isclose(critic_bias_before, critic_bias_after).all()
-    assert th.isclose(critic_running_mean_before, critic_running_mean_after).all()
-
-    assert th.isclose(critic_target_bias_before, critic_target_bias_after).all()
-    assert th.isclose(critic_target_running_mean_before, critic_target_running_mean_after).all()
 
 
 @pytest.mark.parametrize("model_class", [A2C, PPO])
@@ -539,15 +341,14 @@ def test_a2c_ppo_predict_with_batch_norm(model_class, env_id):
         seed=1,
     )
 
-    batch_norm = model.policy.features_extractor.batch_norm
-    bias_before, running_mean_before = clone_batch_norm_stats(batch_norm)
+    bias_before, running_mean_before = clone_on_policy_batch_norm(model)
 
     env = model.get_env()
     observation = env.reset()
-    for _ in range(10):
+    for _ in range(5):
         model.predict(observation, deterministic=True)
 
-    bias_after, running_mean_after = clone_batch_norm_stats(batch_norm)
+    bias_after, running_mean_after = clone_on_policy_batch_norm(model)
 
     assert th.isclose(bias_before, bias_after).all()
     assert th.isclose(running_mean_before, running_mean_after).all()
@@ -555,7 +356,7 @@ def test_a2c_ppo_predict_with_batch_norm(model_class, env_id):
 
 @pytest.mark.parametrize("model_class", MODEL_LIST)
 @pytest.mark.parametrize("env_id", ["Pendulum-v0", "CartPole-v1"])
-def test_predict_with_dropout(model_class, env_id):
+def test_predict_with_dropout_batch_norm(model_class, env_id):
     if env_id == "CartPole-v1":
         if model_class in [SAC, TD3]:
             return
@@ -563,6 +364,7 @@ def test_predict_with_dropout(model_class, env_id):
         return
 
     model_kwargs = dict(seed=1)
+    clone_helper = CLONE_HELPERS[model_class]
 
     if model_class in [DQN, TD3, SAC]:
         model_kwargs["learning_starts"] = 0
@@ -575,9 +377,17 @@ def test_predict_with_dropout(model_class, env_id):
     )
     model = model_class("MlpPolicy", env_id, policy_kwargs=policy_kwargs, verbose=1, **model_kwargs)
 
+    batch_norm_stats_before = clone_helper(model)
+
     env = model.get_env()
     observation = env.reset()
     first_prediction, _ = model.predict(observation, deterministic=True)
-    for _ in range(10):
+    for _ in range(5):
         prediction, _ = model.predict(observation, deterministic=True)
         np.testing.assert_allclose(first_prediction, prediction)
+
+    batch_norm_stats_after = clone_helper(model)
+
+    # No change in batch norm params
+    for param_before, param_after in zip(batch_norm_stats_before, batch_norm_stats_after):
+        assert th.isclose(param_before, param_after).all()

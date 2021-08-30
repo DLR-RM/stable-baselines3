9dfbb3b866a9a2482319f88900270f8fb917b0b8
Scott Brownlie <scott.brownlie@farfetch.com>
2021-08-30 09:10:44 +0100

    remove Optional typing for batch size - causing type check to fail

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -70,7 +70,7 @@ class PPO(OnPolicyAlgorithm):
         env: Union[GymEnv, str],
         learning_rate: Union[float, Schedule] = 3e-4,
         n_steps: int = 2048,
-        batch_size: Optional[int] = 64,
+        batch_size: int = 64,
         n_epochs: int = 10,
         gamma: float = 0.99,
         gae_lambda: float = 0.95,

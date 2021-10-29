6f9503a29dd7e1c655b336cb094a308d5109e76f
Oleksii Kachaiev <okachaiev@riotgames.com>
2021-10-28 23:09:41 -0700

    Store number of timesteps at the beginning of each learn cycle

--- stable_baselines3/common/base_class.py
+++ stable_baselines3/common/base_class.py
@@ -420,6 +420,7 @@ class BaseAlgorithm(ABC):
             # Make sure training timesteps are ahead of the internal counter
             total_timesteps += self.num_timesteps
         self._total_timesteps = total_timesteps
+        self._num_timesteps_at_start = self.num_timesteps
 
         # Avoid resetting the environment when calling ``.learn()`` consecutive times
         if reset_num_timesteps or self._last_obs is None:

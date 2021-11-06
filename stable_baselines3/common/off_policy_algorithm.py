81b294766ce15bca42d286d9a1bac1542b48757e
Antonin Raffin <antonin.raffin@ensta.org>
2021-11-06 22:44:44 +0100

    Bug fix with VecNormalize

--- stable_baselines3/common/off_policy_algorithm.py
+++ stable_baselines3/common/off_policy_algorithm.py
@@ -500,7 +500,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                     next_obs_ = infos[i]["terminal_observation"]
                     # VecNormalize normalizes the terminal observation
                     if self._vec_normalize_env is not None:
-                        next_obs_ = self._vec_normalize_env.unnormalize_obs(next_obs)
+                        next_obs_ = self._vec_normalize_env.unnormalize_obs(next_obs_)
                     # Replace next obs for the correct envs
                     for key in next_obs.keys():
                         next_obs[key][i] = next_obs_[key]

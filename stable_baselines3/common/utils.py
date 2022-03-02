a730d4009008110e92e3a2419347b7223a64d241
James Hirschorn <James.Hirschorn@quantitative-technologies.com>
2022-03-02 10:57:22 -0500

    escape tensorboard log name
    Otherwise utils does not recognize the log.

--- stable_baselines3/common/utils.py
+++ stable_baselines3/common/utils.py
@@ -162,7 +162,7 @@ def get_latest_run_id(log_path: Optional[str] = None, log_name: str = "") -> int
     :return: latest run number
     """
     max_run_id = 0
-    for path in glob.glob(f"{log_path}/{log_name}_[0-9]*"):
+    for path in glob.glob(f"{log_path}/{glob.escape(log_name)}_[0-9]*"):
         file_name = path.split(os.sep)[-1]
         ext = file_name.split("_")[-1]
         if log_name == "_".join(file_name.split("_")[:-1]) and ext.isdigit() and int(ext) > max_run_id:

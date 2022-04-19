de3f086fcd0ec155fc525d95d266847d1deb15ce
Antonin Raffin <antonin.raffin@ensta.org>
2022-04-19 09:53:30 +0200

    Fix randint

--- stable_baselines3/common/atari_wrappers.py
+++ stable_baselines3/common/atari_wrappers.py
@@ -33,7 +33,7 @@ class NoopResetEnv(gym.Wrapper):
         if self.override_num_noops is not None:
             noops = self.override_num_noops
         else:
-            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1)
+            noops = self.unwrapped.np_random.integers(1, self.noop_max + 1)
         assert noops > 0
         obs = np.zeros(0)
         for _ in range(noops):

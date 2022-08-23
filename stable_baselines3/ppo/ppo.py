7efe2db2bec460ea1a5a3c9ea65e7b921bc3c4d1
Hugh Perkins <hughperkins@gmail.com>
2022-08-23 19:01:28 -0400

    fix nan in advnatages with batch size 1, for ppo

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -211,7 +211,8 @@ class PPO(OnPolicyAlgorithm):
                 # Normalize advantage
                 advantages = rollout_data.advantages
                 if self.normalize_advantage:
-                    advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+                    batch_size = len(actions)
+                    advantages = (advantages - advantages.mean()) / (advantages.std(unbiased=batch_size > 1) + 1e-8)
 
                 # ratio between old and new policy, should be one at the first iteration
                 ratio = th.exp(log_prob - rollout_data.old_log_prob)

5971e139287a59f391a3b00b216208979486a593
Hugh Perkins <hughperkins@gmail.com>
2022-08-23 19:06:00 -0400

    black

--- tests/test_ppo.py
+++ tests/test_ppo.py
@@ -1,9 +1,11 @@
+from unittest import mock
+
+import numpy as np
+import torch as th
+from gym import spaces
+
 from stable_baselines3 import PPO
 from stable_baselines3.common import buffers
-import torch as th
-from unittest import mock
-from gym import spaces
-import numpy as np
 
 
 def test_batch_size_1_not_nan():
@@ -15,12 +17,16 @@ def test_batch_size_1_not_nan():
     self.n_epochs = 1
     obs_space = spaces.Discrete(3)
     action_space = spaces.Discrete(2)
-    rollout_buffer = buffers.RolloutBuffer(
-        buffer_size=2, observation_space=obs_space, action_space=action_space)
+    rollout_buffer = buffers.RolloutBuffer(buffer_size=2, observation_space=obs_space, action_space=action_space)
     for _ in range(2):
         rollout_buffer.add(
-            obs=np.zeros(1), action=np.zeros(1), reward=np.zeros(1), episode_start=np.zeros(1),
-            value=th.zeros(1), log_prob=th.zeros(1))
+            obs=np.zeros(1),
+            action=np.zeros(1),
+            reward=np.zeros(1),
+            episode_start=np.zeros(1),
+            value=th.zeros(1),
+            log_prob=th.zeros(1),
+        )
     rollout_buffer.compute_returns_and_advantage(last_values=th.zeros(1), dones=np.zeros(1))
     self.rollout_buffer = rollout_buffer
     self.batch_size = 1
@@ -36,17 +42,16 @@ def test_batch_size_1_not_nan():
 
     self.policy = mock.Mock()
     self.policy.parameters.return_value = []
-    self.policy.evaluate_actions.return_value = (
-        th.zeros(1, requires_grad=True), th.zeros(1, requires_grad=True), th.zeros(1)
-    )
+    self.policy.evaluate_actions.return_value = (th.zeros(1, requires_grad=True), th.zeros(1, requires_grad=True), th.zeros(1))
     self.policy.log_std = th.zeros(1)
 
     loss = None
 
     def record(key, value, **kwargs):
         nonlocal loss
-        if key == 'train/loss':
+        if key == "train/loss":
             loss = value
+
     self.logger.record = record
 
     PPO.train(self=self)

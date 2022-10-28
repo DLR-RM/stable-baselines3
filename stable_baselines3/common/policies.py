597b5847bfd264e044a1c5e6aa517f53a294f967
Adam Gleave <adam@gleave.me>
2022-10-28 14:39:49 -0700

    Add PolicyPredictor protocol and use it in evaluate_policy

--- stable_baselines3/common/policies.py
+++ stable_baselines3/common/policies.py
@@ -4,7 +4,7 @@ import collections
 import copy
 from abc import ABC, abstractmethod
 from functools import partial
-from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, Union
+from typing import Any, Dict, List, Optional, Protocol, Tuple, Type, TypeVar, Union
 
 import gym
 import numpy as np
@@ -242,6 +242,29 @@ class BaseModel(nn.Module):
 
         observation = obs_as_tensor(observation, self.device)
         return observation, vectorized_env
+
+
+class PolicyPredictor(Protocol):
+    def predict(
+        self,
+        observation: Union[np.ndarray, Dict[str, np.ndarray]],
+        state: Optional[Tuple[np.ndarray, ...]] = None,
+        episode_start: Optional[np.ndarray] = None,
+        deterministic: bool = False,
+    ) -> Tuple[np.ndarray, Optional[Tuple[np.ndarray, ...]]]:
+        """
+        Get the policy action from an observation (and optional hidden state).
+        Includes sugar-coating to handle different observations (e.g. normalizing images).
+
+        :param observation: the input observation
+        :param state: The last hidden states (can be None, used in recurrent policies)
+        :param episode_start: The last masks (can be None, used in recurrent policies)
+            this correspond to beginning of episodes,
+            where the hidden states of the RNN must be reset.
+        :param deterministic: Whether or not to return deterministic actions.
+        :return: the model's action and the next hidden state
+            (used in recurrent policies)
+        """
 
 
 class BasePolicy(BaseModel, ABC):

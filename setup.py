767d70fc06063b7924464d5f4c355290808a3d65
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-21 10:58:04 +0100

    Drop 3.11 (because of pytorch)

--- setup.py
+++ setup.py
@@ -155,7 +155,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
     ],
 )
 

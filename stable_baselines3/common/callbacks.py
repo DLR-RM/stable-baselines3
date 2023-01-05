7bb83f5e7cf314b445b33f3c0fda602cc17b12f6
dominicgkerr <dominicgkerr1@gmail.co>
2023-01-05 00:46:03 +0000

    Updated tqdm progress bar constructor to account for the effects of train_freq/n_steps/num_envs on total_timesteps. Ensure progress bar is "flushed" on training end.

--- stable_baselines3/common/callbacks.py
+++ stable_baselines3/common/callbacks.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 from abc import ABC, abstractmethod
+from math import ceil
 from typing import Any, Callable, Dict, List, Optional, Union
 
 import gym
@@ -675,9 +676,36 @@ class ProgressBarCallback(BaseCallback):
         self.pbar = None
 
     def _on_training_start(self) -> None:
-        # Initialize progress bar
+
+        # Get the total number of timesteps to do this training session
+        total = self.locals["total_timesteps"]
+
         # Remove timesteps that were done in previous training sessions
-        self.pbar = tqdm(total=self.locals["total_timesteps"] - self.model.num_timesteps)
+        total -= self.model.num_timesteps
+
+        # Round timesteps up-to a multiple of the algorithm's batch size
+        # (i.e. the number steps generated when gaining experience or the
+        # interval between training updates, multiplied by the number of
+        # environments)
+        batch_size = self.training_env.num_envs
+        if hasattr(self.model, "n_steps"):
+            batch_size *= self.model.n_steps
+
+        elif hasattr(self.model, "train_freq"):
+            # Avoids circular import
+            from stable_baselines3.common.type_aliases import TrainFrequencyUnit
+
+            if self.model.train_freq.unit == TrainFrequencyUnit.STEP:
+                batch_size *= self.model.train_freq.frequency
+
+            # When unit is TrainFrequencyUnit.EPISODE, there is no guarantee that
+            # a fixed number of steps will be processed (e.g. episodes may
+            # have variable length)
+
+        total = ceil(total / batch_size) * batch_size
+
+        # Finally, initialize progress bar
+        self.pbar = tqdm(total=total)
 
     def _on_step(self) -> bool:
         # Update progress bar, we do num_envs steps per call to `env.step()`
@@ -685,5 +713,6 @@ class ProgressBarCallback(BaseCallback):
         return True
 
     def _on_training_end(self) -> None:
-        # Close progress bar
+        # Flush and close progress bar
+        self.pbar.refresh()
         self.pbar.close()

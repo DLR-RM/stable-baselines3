720e95b8d326af5ece288d9f8d8428e5aa2186f6
WeberSamuel <37903003+WeberSamuel@users.noreply.github.com>
2023-04-11 18:20:35 +0200

    Improved test

--- tests/test_vec_extract_dict_obs.py
+++ tests/test_vec_extract_dict_obs.py
@@ -39,7 +39,12 @@ def test_extract_dict_obs():
     env = DictObsVecEnv()
     env = VecExtractDictObs(env, "rgb")
     assert env.reset().shape == (4, 86, 86)
-    env.step(None)
+    
+    obs, _, _, infos = env.step([env.action_space.sample() for _ in range(env.num_envs)])
+    assert obs.shape == (4, 86, 86)
+    for info in infos:
+        if "terminal_observation" in info:
+            assert info["terminal_observation"].shape == (86, 86)
 
 
 def test_vec_with_ppo():

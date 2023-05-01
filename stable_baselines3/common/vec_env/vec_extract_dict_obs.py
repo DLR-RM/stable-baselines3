762454763bd1759128ae33d569b3b2797d8ef55f
Antonin Raffin <antonin.raffin@ensta.org>
2023-05-01 13:36:49 +0200

    Fix vec extract dict obs type hints

--- stable_baselines3/common/vec_env/vec_extract_dict_obs.py
+++ stable_baselines3/common/vec_env/vec_extract_dict_obs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from gymnasium import spaces
 
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvStepReturn, VecEnvWrapper
 
@@ -13,14 +14,19 @@ class VecExtractDictObs(VecEnvWrapper):
 
     def __init__(self, venv: VecEnv, key: str):
         self.key = key
+        assert isinstance(
+            venv.observation_space, spaces.Dict
+        ), f"VecExtractDictObs can only be used with Dict obs space, not {venv.observation_space}"
         super().__init__(venv=venv, observation_space=venv.observation_space.spaces[self.key])
 
     def reset(self) -> np.ndarray:
         obs = self.venv.reset()
+        assert isinstance(obs, dict)
         return obs[self.key]
 
     def step_wait(self) -> VecEnvStepReturn:
         obs, reward, done, infos = self.venv.step_wait()
+        assert isinstance(obs, dict)
         for info in infos:
             if "terminal_observation" in info:
                 info["terminal_observation"] = info["terminal_observation"][self.key]

b2b0ff619635cd4af5969e369758cb52fe5c48e6
tobirohrer <tobias.rohrer@outlook.com>
2023-06-22 22:35:41 +0200

    making test more restrictive by checking models action is float64

--- tests/test_spaces.py
+++ tests/test_spaces.py
@@ -146,4 +146,8 @@ def test_discrete_obs_space(model_class, env):
 def test_float64_support(model_class):
     env = DummyContinuousActionFloat64()
     env = gym.wrappers.TimeLimit(env, max_episode_steps=200)
-    model_class("MlpPolicy", env).learn(20)
+    model = model_class("MlpPolicy", env)
+    model.learn(20)
+    initial_obs, _ = env.reset()
+    action, _ = model.predict(initial_obs)
+    assert action.dtype == np.float64

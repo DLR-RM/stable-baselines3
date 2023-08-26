6766048f7e1f9b96099b75fd241d61c31e71bb25
PatrickHelm <patrick.helm@gmx.net>
2023-08-26 10:47:29 +0200

    replaces full env with wrapper

--- tests/test_sde.py
+++ tests/test_sde.py
@@ -1,13 +1,9 @@
-from os import path
 from typing import List, Optional
 
-import gymnasium as gym
 import numpy as np
 import pytest
 import torch as th
-from gymnasium import spaces
-from gymnasium.envs.classic_control import utils
-from gymnasium.error import DependencyNotInstalled
+from gymnasium.envs.classic_control.pendulum import PendulumEnv
 from torch.distributions import Normal
 
 from stable_baselines3 import A2C, PPO, SAC
@@ -76,7 +72,7 @@ def test_state_dependent_noise(model_class, use_expln, squash_output):
         policy_kwargs = dict(log_std_init=-2, use_expln=use_expln, net_arch=[64])
     else:
         policy_kwargs = dict(log_std_init=-2, use_expln=use_expln, net_arch=[64], squash_output=squash_output)
-    env = PendulumEnv()
+    env = PendulumEnvWrapper()
     model = model_class(
         "MlpPolicy",
         env,
@@ -101,164 +97,13 @@ def test_state_dependent_noise(model_class, use_expln, squash_output):
 
 
 # default pendulum env from gymnasium with action tracking
-DEFAULT_X = np.pi
-DEFAULT_Y = 1.0
-
-
-class PendulumEnv(gym.Env):
-    """
-    ## Description
-
-    The inverted pendulum swingup problem is based on the classic problem in control theory.
-    The system consists of a pendulum attached at one end to a fixed point, and the other end being free.
-    The pendulum starts in a random position and the goal is to apply torque on the free end to swing it
-    into an upright position, with its center of gravity right above the fixed point.
-
-    The diagram below specifies the coordinate system used for the implementation of the pendulum's
-    dynamic equations.
-
-    ![Pendulum Coordinate System](/_static/diagrams/pendulum.png)
-
-    -  `x-y`: cartesian coordinates of the pendulum's end in meters.
-    - `theta` : angle in radians.
-    - `tau`: torque in `N m`. Defined as positive _counter-clockwise_.
-
-    ## Action Space
-
-    The action is a `ndarray` with shape `(1,)` representing the torque applied to free end of the pendulum.
-
-    | Num | Action | Min  | Max |
-    |-----|--------|------|-----|
-    | 0   | Torque | -2.0 | 2.0 |
-
-
-    ## Observation Space
-
-    The observation is a `ndarray` with shape `(3,)` representing the x-y coordinates of the pendulum's free
-    end and its angular velocity.
-
-    | Num | Observation      | Min  | Max |
-    |-----|------------------|------|-----|
-    | 0   | x = cos(theta)   | -1.0 | 1.0 |
-    | 1   | y = sin(theta)   | -1.0 | 1.0 |
-    | 2   | Angular Velocity | -8.0 | 8.0 |
-
-    ## Rewards
-
-    The reward function is defined as:
-
-    *r = -(theta<sup>2</sup> + 0.1 * theta_dt<sup>2</sup> + 0.001 * torque<sup>2</sup>)*
-
-    where `theta` is the pendulum's angle normalized between *[-pi, pi]* (with 0 being in the upright position).
-    Based on the above equation, the minimum reward that can be obtained is
-    *-(pi<sup>2</sup> + 0.1 * 8<sup>2</sup> + 0.001 * 2<sup>2</sup>) = -16.2736044*,
-    while the maximum reward is zero (pendulum is upright with zero velocity and no torque applied).
-
-    ## Starting State
-
-    The starting state is a random angle in *[-pi, pi]* and a random angular velocity in *[-1,1]*.
-
-    ## Episode Truncation
-
-    The episode truncates at 200 time steps.
-
-    ## Arguments
-
-    - `g`: acceleration of gravity measured in *(m s<sup>-2</sup>)* used to calculate the pendulum dynamics.
-      The default value is g = 10.0 .
-
-    ```python
-    import gymnasium as gym
-    gym.make('Pendulum-v1', g=9.81)
-    ```
-
-    On reset, the `options` parameter allows the user to change the bounds used to determine
-    the new random state.
-
-    ## Version History
-
-    * v1: Simplify the math equations, no difference in behavior.
-    * v0: Initial versions release (1.0.0)
-
-    """
-
-    metadata = {
-        "render_modes": ["human", "rgb_array"],
-        "render_fps": 30,
-    }
-
+class PendulumEnvWrapper(PendulumEnv):
     def __init__(self, render_mode: Optional[str] = None, g=10.0):
-        self.max_speed = 8
-        self.max_torque = 2.0
-        self.dt = 0.05
-        self.g = g
-        self.m = 1.0
-        self.l = 1.0
-
-        self.render_mode = render_mode
-
-        self.screen_dim = 500
-        self.screen = None
-        self.clock = None
-        self.isopen = True
-
-        high = np.array([1.0, 1.0, self.max_speed], dtype=np.float32)
-        # This will throw a warning in tests/envs/test_envs in utils/env_checker.py as the space is not symmetric
-        #   or normalised as max_torque == 2 by default. Ignoring the issue here as the default settings are too old
-        #   to update to follow the gymnasium api
-        self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
-
-        # added for tracking actions
+        super().__init__(render_mode, g)
+        # defines list for tracking actions
         self.actions: List[float] = []
 
     def step(self, u):
-        # added for tracking actions
+        # appends list for tracking actions
         self.actions.append(u)
-
-        th, thdot = self.state  # th := theta
-
-        g = self.g
-        m = self.m
-        l = self.l
-        dt = self.dt
-
-        u = np.clip(u, -self.max_torque, self.max_torque)[0]
-        self.last_u = u  # for rendering
-        costs = angle_normalize(th) ** 2 + 0.1 * thdot**2 + 0.001 * (u**2)
-
-        newthdot = thdot + (3 * g / (2 * l) * np.sin(th) + 3.0 / (m * l**2) * u) * dt
-        newthdot = np.clip(newthdot, -self.max_speed, self.max_speed)
-        newth = th + newthdot * dt
-
-        self.state = np.array([newth, newthdot])
-
-        if self.render_mode == "human":
-            self.render()
-        return self._get_obs(), -costs, False, False, {}
-
-    def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
-        super().reset(seed=seed)
-        if options is None:
-            high = np.array([DEFAULT_X, DEFAULT_Y])
-        else:
-            # Note that if you use custom reset bounds, it may lead to out-of-bound
-            # state/observations.
-            x = DEFAULT_X
-            y = DEFAULT_Y
-            high = np.array([x, y])
-        low = -high  # We enforce symmetric limits.
-        self.state = self.np_random.uniform(low=low, high=high)
-        self.last_u = None
-
-        if self.render_mode == "human":
-            self.render()
-        return self._get_obs(), {}
-
-    def _get_obs(self):
-        theta, thetadot = self.state
-        return np.array([np.cos(theta), np.sin(theta), thetadot], dtype=np.float32)
-
-
-def angle_normalize(x):
-    return ((x + np.pi) % (2 * np.pi)) - np.pi
+        return super().step(u)

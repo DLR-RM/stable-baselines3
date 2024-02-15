22e75dd6c5b4c4cf09f07e0d39b0d0f405758ebd
Digsno <36057459+jgiral95@users.noreply.github.com>
2024-02-15 00:25:06 -0500

    Update dqn.py
    Update dqn.py to fix problem with action spaces that do not start at 0.

--- stable_baselines3/dqn/dqn.py
+++ stable_baselines3/dqn/dqn.py
@@ -206,7 +206,7 @@ class DQN(OffPolicyAlgorithm):
             current_q_values = self.q_net(replay_data.observations)
 
             # Retrieve the q-values for the actions from the replay buffer
-            current_q_values = th.gather(current_q_values, dim=1, index=replay_data.actions.long())
+            current_q_values = th.gather(current_q_values, dim=1, index=(replay_data.actions.long()-self.env.action_space.start))
 
             # Compute Huber loss (less sensitive to outliers)
             loss = F.smooth_l1_loss(current_q_values, target_q_values)

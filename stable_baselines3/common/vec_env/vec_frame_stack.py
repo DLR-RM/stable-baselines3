e1910d40d04818b9ddb68c934677a8a5b6a80f31
corentinlger <corentin.lger@gmail.com>
2024-03-20 10:56:00 +0100

    Automatic refactoring of the type hinting

--- stable_baselines3/common/vec_env/vec_frame_stack.py
+++ stable_baselines3/common/vec_env/vec_frame_stack.py
@@ -29,12 +29,7 @@ class VecFrameStack(VecEnvWrapper):
 
     def step_wait(
         self,
-    ) -> Tuple[
-        Union[np.ndarray, Dict[str, np.ndarray]],
-        np.ndarray,
-        np.ndarray,
-        List[Dict[str, Any]],
-    ]:
+    ) -> Tuple[Union[np.ndarray, Dict[str, np.ndarray]], np.ndarray, np.ndarray, List[Dict[str, Any]],]:
         observations, rewards, dones, infos = self.venv.step_wait()
         observations, infos = self.stacked_obs.update(observations, dones, infos)  # type: ignore[arg-type]
         return observations, rewards, dones, infos

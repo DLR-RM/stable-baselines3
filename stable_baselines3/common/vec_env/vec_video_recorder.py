45cd5f8eb628b14d433d2142385d6dcc71a3653d
Antonin RAFFIN <antonin.raffin@ensta.org>
2024-11-02 11:08:57 +0100

    Integrate video recorder

--- stable_baselines3/common/vec_env/vec_video_recorder.py
+++ stable_baselines3/common/vec_env/vec_video_recorder.py
@@ -1,8 +1,6 @@
-import json
 import os
 import os.path
-import tempfile
-from typing import Callable, List, Optional
+from typing import Callable, List
 
 import numpy as np
 from gymnasium import error, logger
@@ -12,173 +10,15 @@ from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 
 
-# This is copy and pasted from Gymnasium v0.26.1
-class VideoRecorder:
-    """VideoRecorder renders a nice movie of a rollout, frame by frame.
-
-    It comes with an ``enabled`` option, so you can still use the same code on episodes where you don't want to record video.
-
-    Note:
-        You are responsible for calling :meth:`close` on a created VideoRecorder, or else you may leak an encoder process.
-    """
-
-    def __init__(
-        self,
-        env,
-        path: Optional[str] = None,
-        metadata: Optional[dict] = None,
-        enabled: bool = True,
-        base_path: Optional[str] = None,
-    ):
-        """Video recorder renders a nice movie of a rollout, frame by frame.
-
-        Args:
-            env (Env): Environment to take video of.
-            path (Optional[str]): Path to the video file; will be randomly chosen if omitted.
-            metadata (Optional[dict]): Contents to save to the metadata file.
-            enabled (bool): Whether to actually record video, or just no-op (for convenience)
-            base_path (Optional[str]): Alternatively, path to the video file without extension, which will be added.
-
-        Raises:
-            Error: You can pass at most one of `path` or `base_path`
-            Error: Invalid path given that must have a particular file extension
-        """
-        try:
-            # check that moviepy is now installed
-            import moviepy  # noqa: F401
-        except ImportError as e:
-            raise error.DependencyNotInstalled("MoviePy is not installed, run `pip install moviepy`") from e
-
-        self._async = env.metadata.get("semantics.async")
-        self.enabled = enabled
-        self._closed = False
-
-        self.render_history: List[np.ndarray] = []
-        self.env = env
-
-        self.render_mode = env.render_mode
-
-        if "rgb_array_list" != self.render_mode and "rgb_array" != self.render_mode:
-            logger.warn(
-                f"Disabling video recorder because environment {env} was not initialized with any compatible video "
-                "mode between `rgb_array` and `rgb_array_list`"
-            )
-            # Disable since the environment has not been initialized with a compatible `render_mode`
-            self.enabled = False
-
-        # Don't bother setting anything else if not enabled
-        if not self.enabled:
-            return
-
-        if path is not None and base_path is not None:
-            raise error.Error("You can pass at most one of `path` or `base_path`.")
-
-        required_ext = ".mp4"
-        if path is None:
-            if base_path is not None:
-                # Base path given, append ext
-                path = base_path + required_ext
-            else:
-                # Otherwise, just generate a unique filename
-                with tempfile.NamedTemporaryFile(suffix=required_ext) as f:
-                    path = f.name
-        self.path = path
-
-        path_base, actual_ext = os.path.splitext(self.path)
-
-        if actual_ext != required_ext:
-            raise error.Error(f"Invalid path given: {self.path} -- must have file extension {required_ext}.")
-
-        self.frames_per_sec = env.metadata.get("render_fps", 30)
-
-        self.broken = False
-
-        # Dump metadata
-        self.metadata = metadata or {}
-        self.metadata["content_type"] = "video/mp4"
-        self.metadata_path = f"{path_base}.meta.json"
-        self.write_metadata()
-
-        # logger.debug(f"Starting new video recorder writing to {self.path}")
-        self.recorded_frames: List[np.ndarray] = []
-
-    @property
-    def functional(self):
-        """Returns if the video recorder is functional, is enabled and not broken."""
-        return self.enabled and not self.broken
-
-    def capture_frame(self):
-        """Render the given `env` and add the resulting frame to the video."""
-        frame = self.env.render()
-        if isinstance(frame, List):
-            self.render_history += frame
-            frame = frame[-1]
-
-        if not self.functional:
-            return
-        if self._closed:
-            logger.warn("The video recorder has been closed and no frames will be captured anymore.")
-            return
-        # logger.debug(f"Capturing video frame: path={self.path}")
-
-        if frame is None:
-            if self._async:
-                return
-            else:
-                # Indicates a bug in the environment: don't want to raise
-                # an error here.
-                logger.warn(
-                    "Env returned None on `render()`. Disabling further rendering for video recorder by marking as "
-                    f"disabled: path={self.path} metadata_path={self.metadata_path}"
-                )
-                self.broken = True
-        else:
-            self.recorded_frames.append(frame)
-
-    def close(self):
-        """Flush all data to disk and close any open frame encoders."""
-        if not self.enabled or self._closed:
-            return
-
-        # First close the environment
-        self.env.close()
-
-        # Close the encoder
-        if len(self.recorded_frames) > 0:
-            try:
-                from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
-            except ImportError as e:
-                raise error.DependencyNotInstalled("MoviePy is not installed, run `pip install moviepy`") from e
-
-            # logger.debug(f"Closing video encoder: path={self.path}")
-            clip = ImageSequenceClip(self.recorded_frames, fps=self.frames_per_sec)
-            clip.write_videofile(self.path)
-        else:
-            # No frames captured. Set metadata.
-            if self.metadata is None:
-                self.metadata = {}
-            self.metadata["empty"] = True
-
-        self.write_metadata()
-
-        # Stop tracking this for autoclose
-        self._closed = True
-
-    def write_metadata(self):
-        """Writes metadata to metadata path."""
-        with open(self.metadata_path, "w") as f:
-            json.dump(self.metadata, f)
-
-    def __del__(self):
-        """Closes the environment correctly when the recorder is deleted."""
-        # Make sure we've closed up shop when garbage collecting
-        self.close()
-
-
 class VecVideoRecorder(VecEnvWrapper):
     """
     Wraps a VecEnv or VecEnvWrapper object to record rendered image as mp4 video.
     It requires ffmpeg or avconv to be installed on the machine.
+
+    Note: for now it only allows to record one video and all videos
+    must have at least two frames.
+
+    The video recorder code was adapted from Gymnasium v1.0.
 
     :param venv:
     :param video_folder: Where to save videos
@@ -188,8 +28,6 @@ class VecVideoRecorder(VecEnvWrapper):
     :param video_length:  Length of recorded videos
     :param name_prefix: Prefix to the video name
     """
-
-    video_recorder: VideoRecorder
 
     def __init__(
         self,
@@ -218,6 +56,8 @@ class VecVideoRecorder(VecEnvWrapper):
         self.env.metadata = metadata
         assert self.env.render_mode == "rgb_array", f"The render_mode must be 'rgb_array', not {self.env.render_mode}"
 
+        self.frames_per_sec = self.env.metadata.get("render_fps", 30)
+
         self.record_video_trigger = record_video_trigger
         self.video_folder = os.path.abspath(video_folder)
         # Create output folder if needed
@@ -227,52 +67,88 @@ class VecVideoRecorder(VecEnvWrapper):
         self.step_id = 0
         self.video_length = video_length
 
+        self.video_name = f"{self.name_prefix}-step-{self.step_id}-to-step-{self.step_id + self.video_length}.mp4"
+        self.video_path = os.path.join(self.video_folder, self.video_name)
+
         self.recording = False
-        self.recorded_frames = 0
+        self.recorded_frames: list[np.ndarray] = []
+
+        try:
+            import moviepy  # noqa: F401
+        except ImportError as e:
+            raise error.DependencyNotInstalled("MoviePy is not installed, run `pip install 'gymnasium[other]'`") from e
 
     def reset(self) -> VecEnvObs:
         obs = self.venv.reset()
-        self.start_video_recorder()
+        if self._video_enabled():
+            self._start_video_recorder()
         return obs
 
-    def start_video_recorder(self) -> None:
-        self.close_video_recorder()
-
-        video_name = f"{self.name_prefix}-step-{self.step_id}-to-step-{self.step_id + self.video_length}"
-        base_path = os.path.join(self.video_folder, video_name)
-        self.video_recorder = VideoRecorder(env=self.env, base_path=base_path, metadata={"step_id": self.step_id})
-
-        self.video_recorder.capture_frame()
-        self.recorded_frames = 1
-        self.recording = True
+    def _start_video_recorder(self) -> None:
+        self._start_recording()
+        self._capture_frame()
 
     def _video_enabled(self) -> bool:
         return self.record_video_trigger(self.step_id)
 
     def step_wait(self) -> VecEnvStepReturn:
-        obs, rews, dones, infos = self.venv.step_wait()
+        obs, rewards, dones, infos = self.venv.step_wait()
 
         self.step_id += 1
         if self.recording:
-            self.video_recorder.capture_frame()
-            self.recorded_frames += 1
-            if self.recorded_frames > self.video_length:
-                print(f"Saving video to {self.video_recorder.path}")
-                self.close_video_recorder()
+            self._capture_frame()
+            if len(self.recorded_frames) > self.video_length:
+                print(f"Saving video to {self.video_path}")
+                self._stop_recording()
         elif self._video_enabled():
-            self.start_video_recorder()
+            self._start_video_recorder()
 
-        return obs, rews, dones, infos
+        return obs, rewards, dones, infos
 
-    def close_video_recorder(self) -> None:
-        if self.recording:
-            self.video_recorder.close()
-        self.recording = False
-        self.recorded_frames = 1
+    def _capture_frame(self) -> None:
+        assert self.recording, "Cannot capture a frame, recording wasn't started."
+
+        frame = self.env.render()
+        if isinstance(frame, List):
+            frame = frame[-1]
+
+        if isinstance(frame, np.ndarray):
+            self.recorded_frames.append(frame)
+        else:
+            self._stop_recording()
+            logger.warn(
+                f"Recording stopped: expected type of frame returned by render to be a numpy array, got instead {type(frame)}."
+            )
 
     def close(self) -> None:
+        """Closes the wrapper then the video recorder."""
         VecEnvWrapper.close(self)
-        self.close_video_recorder()
+        if self.recording:
+            self._stop_recording()
 
-    def __del__(self):
-        self.close_video_recorder()
+    def _start_recording(self) -> None:
+        """Start a new recording. If it is already recording, stops the current recording before starting the new one."""
+        if self.recording:
+            self._stop_recording()
+
+        self.recording = True
+
+    def _stop_recording(self) -> None:
+        """Stop current recording and saves the video."""
+        assert self.recording, "_stop_recording was called, but no recording was started"
+
+        if len(self.recorded_frames) == 0:
+            logger.warn("Ignored saving a video as there were zero frames to save.")
+        else:
+            from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+
+            clip = ImageSequenceClip(self.recorded_frames, fps=self.frames_per_sec)
+            clip.write_videofile(self.video_path)
+
+        self.recorded_frames = []
+        self.recording = False
+
+    def __del__(self) -> None:
+        """Warn the user in case last video wasn't saved."""
+        if len(self.recorded_frames) > 0:
+            logger.warn("Unable to save last video! Did you call close()?")

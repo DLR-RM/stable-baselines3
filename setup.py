3f73e629653e9cdec5c41bc1f51b4387910a7e49
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2025-03-03 10:21:44 +0000

    Add support for Gymnasium v1.1.0

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     packages=[package for package in find_packages() if package.startswith("stable_baselines3")],
     package_data={"stable_baselines3": ["py.typed", "version.txt"]},
     install_requires=[
-        "gymnasium>=0.29.1,<1.1.0",
+        "gymnasium>=0.29.1,<1.2.0",
         "numpy>=1.20,<3.0",
         "torch>=2.3,<3.0",
         # For saving models

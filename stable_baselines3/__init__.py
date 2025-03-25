5b6f908aea840e78e700b62fce4c99dacbbbc232
aarunsrinivas5 <aarun5@umd.edu>
2025-03-24 20:34:01 -0400

    Add Risk Sensitive PPO (#1)

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -8,6 +8,9 @@ from stable_baselines3.her.her_replay_buffer import HerReplayBuffer
 from stable_baselines3.ppo import PPO
 from stable_baselines3.sac import SAC
 from stable_baselines3.td3 import TD3
+from stable_baselines3.rsppo import RSPPO
+# from stable_baselines3.rsa2c import RSA2C
+# from stable_baselines3.exptd3 import EXPTD3
 
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
@@ -29,6 +32,8 @@ __all__ = [
     "PPO",
     "SAC",
     "TD3",
+    "RSPPO",
+    # "RSA2C",
     "HerReplayBuffer",
     "get_system_info",
 ]

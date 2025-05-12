5839bd6782be96a4f833dbb9bacaa3919ff61774
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2025-05-12 16:41:24 +0200

    use lerp for polyak updates

--- stable_baselines3/common/utils.py
+++ stable_baselines3/common/utils.py
@@ -472,9 +472,7 @@ def polyak_update(
     with th.no_grad():
         # zip does not raise an exception if length of parameters does not match.
         for param, target_param in zip_strict(params, target_params):
-            target_param.data.mul_(1 - tau)
-            th.add(target_param.data, param.data, alpha=tau, out=target_param.data)
-
+            target_param.data.lerp_(param.data, tau)
 
 def obs_as_tensor(obs: Union[np.ndarray, dict[str, np.ndarray]], device: th.device) -> Union[th.Tensor, TensorDict]:
     """

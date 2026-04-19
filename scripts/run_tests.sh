338d9b518bf128f019638049c9f7c72132b1bce9
Antonin RAFFIN <antonin.raffin@ensta.org>
2026-04-19 16:45:01 +0200

    Always report 10 slowest tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m pytest --cov-report html --cov-report term --cov=. -v --color=yes -m "not expensive"
+python3 -m pytest --cov-report html --cov-report term --cov=. -v --color=yes -m "not expensive" --durations=10
